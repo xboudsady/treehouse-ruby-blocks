f90dc395849ed79c787c265cb5af940227c5eec8
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-07-06 17:46:56 -0700

    Initial commit

---
+++ block_examples.rb
@@ -0,0 +1,5 @@
+3.times do
+    puts "Hello world!"
+    puts "This is the second line."
+    true
+end
---
+++ calling_blocks.rb
@@ -0,0 +1,9 @@
+def get_name
+    print prompt + ": "
+    name = gets.chomp
+    yield name                             # yield keyword jumps out of a block function, and returns the variable value
+end
+
+get_name do |name|                          # catches the variable name
+    print "That's a cool name, ! #{name}"
+end
