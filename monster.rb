7052758228ff0dcc2c795de39512e8d0ef6dc4e3
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-07-06 20:51:16 -0700

    Created monster class, and a say method.

--- monster.rb
+++ monster.rb
@@ -1,14 +1,27 @@
 class Monster
-    attr_reader :name               # So we dont have to type the @ sign inside the class
+    attr_reader :name, :actions               # So we dont have to type the @ sign inside the class
     def initialize(name)
         @name = name
+        @actions = {
+            scream: 0
+        }
     end
 
     def say(&block)
         print "#{name} says... "
         yield
     end
+
+    def scream(&block)
+        actions[:screams] += 1
+        print "#{name} screams!"
+        yield
+    end
 end
 
 monster = Monster.new("Fluffy")
 monster.say { puts "Welcome to my home." }
+
+monster.scream do
+    puts "BOO!"
+end
